dfbfa199e77058e023a882c50d2b47814bbd3bc1
adrian <adrian@fury.fan>
2023-06-14 01:42:07 +0530

    comit

--- init_testnet.sh
+++ init_testnet.sh
@@ -15,7 +15,7 @@ TRACE=""
 command -v jq > /dev/null 2>&1 || { echo >&2 "jq not installed. More info: https://stedolan.github.io/jq/download/"; exit 1; }
 
 # Reinstall daemon
-rm -rf ~/.blackfuryd*
+rm -rf ~/.blackfury*
 make install
 
 # Set client config
